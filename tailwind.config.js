89f5d8f8386c7188b81d5069f5537be8a4392c30
Udeorah Frank <franklynudeorah@gmail.com>
2024-02-09 12:48:38 +0100

    install tailwind and react-router-dom

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#3A408C',
+        'primary-100': '#484fb1',
+        'secondary': '#E2342D',
+        'secondary-100': '#DF5853',
+        'grey': '#F3F3F3',
+        'grey-100': '#F3F3F3C0'
+      },
+      maxWidth: {
+        'site': '1200px'
+      }
+    },
+  },
+  plugins: [],
+}
+
